1b05fb4f4809f5829f66a3d9ea5d1ff3f9ac5b70
Nikos S <niksal1991@gmail.com>
2022-05-28 08:53:53 +0300

    Implemented gitignore for already tracked files

--- Lottery/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lottery
-{
-    public class User
-    {
-        public string BlockchainProvider { get; set; }
-        public string MetamaskAddress { get; set; }
-        public string PrivateKey { get; set; }
-    }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# CSharpInWeb3SmartContracts
