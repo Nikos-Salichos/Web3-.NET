dd2cd98a881e0ddd770488607c3954833e8be03c
Nikos S <niksal1991@gmail.com>
2022-05-28 09:43:03 +0300

    Implemented GetBalance() in WalletController

--- CSharpInWeb3SmartContracts/Controllers/WalletController.cs
+++ CSharpInWeb3SmartContracts/Controllers/WalletController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nethereum.Signer;
+using Nethereum.Web3.Accounts;
 
 namespace CSharpInWeb3SmartContracts.Controllers
 {
@@ -11,9 +13,21 @@ namespace CSharpInWeb3SmartContracts.Controllers
 
         private readonly User _user = new User();
 
-        public WalletController()
+        public WalletController(IConfiguration configuration)
         {
+            _configuration = configuration;
+            _user.BlockchainProvider = _configuration["BlockchainProvider"];
+            _user.MetamaskAddress = _configuration["MetamaskAddress"];
+            _user.PrivateKey = _configuration["PrivateKey"];
+        }
+
+
+        [HttpGet("GetBalance")]
+        public async Task<ActionResult> GetBalance()
+        {
+            Account? account = new Account(_user.PrivateKey, Chain.Kovan);
 
         }
+
     }
 }
