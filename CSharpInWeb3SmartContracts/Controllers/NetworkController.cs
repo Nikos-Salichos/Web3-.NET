acae58cb239b1fc928aa9c8e9d19cbd1f9520f50
Nikos S <niksal1991@gmail.com>
2022-06-10 18:45:23 +0300

    Implemented GetAllTransactionOfABlock

--- CSharpInWeb3SmartContracts/Controllers/NetworkController.cs
+++ CSharpInWeb3SmartContracts/Controllers/NetworkController.cs
@@ -41,6 +41,8 @@ namespace CSharpInWeb3SmartContracts.Controllers
             try
             {
                 Account? account = new Account(_user.PrivateKey, chain);
+                Web3? web3 = new Web3(account, _user.BlockchainProvider);
+
 
 
             }
