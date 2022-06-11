e85270042fe3fcc744b7204e267a61a90706172c
Nikos S <niksal1991@gmail.com>
2022-06-11 08:24:37 +0300

    Implemented  TransferTokens

--- CSharpInWeb3SmartContracts/Controllers/WalletController.cs
+++ CSharpInWeb3SmartContracts/Controllers/WalletController.cs
@@ -75,6 +75,8 @@ namespace CSharpInWeb3SmartContracts.Controllers
                 };
 
                 IContractTransactionHandler<TransferFunction> transferHandler = web3.Eth.GetContractTransactionHandler<TransferFunction>();
+                TransactionReceipt? transactionReceipt = await transferHandler.SendRequestAndWaitForReceiptAsync("0xd0a1e359811322d97991e03f863a0c30c2cf029c", transferFunction);
+
             }
             catch (Exception exception)
             {
