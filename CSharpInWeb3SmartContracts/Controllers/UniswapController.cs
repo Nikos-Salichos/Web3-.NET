fdbbee79eca6216efdef64ece1407a142c358806
Nikos S <niksal1991@gmail.com>
2022-06-18 15:35:36 +0300

    Implemented UniswapV2RouterGetAmountsOut

--- CSharpInWeb3SmartContracts/Controllers/UniswapController.cs
+++ CSharpInWeb3SmartContracts/Controllers/UniswapController.cs
@@ -186,11 +186,6 @@ namespace CSharpInWeb3SmartContracts.Controllers
         {
             try
             {
-                /*
-                 [
-                  "0xd0A1E359811322d97991E03f863a0C30C2cF029C","0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa"
-                ]
-                */
                 Account? account = new Account(_user.PrivateKey, Chain.Kovan);
                 Web3? web3 = new Web3(account, _user.BlockchainProviderKovan);
 
