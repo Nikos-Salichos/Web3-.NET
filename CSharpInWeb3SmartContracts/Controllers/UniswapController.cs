d80d41e16aeb5360fc87735281772f63d6f23a87
Nikos S <niksal1991@gmail.com>
2022-06-18 15:34:16 +0300

    Implemented UniswapV2RouterGetAmountsOut

--- CSharpInWeb3SmartContracts/Controllers/UniswapController.cs
+++ CSharpInWeb3SmartContracts/Controllers/UniswapController.cs
@@ -191,6 +191,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
                   "0xd0A1E359811322d97991E03f863a0C30C2cF029C","0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa"
                 ]
                 */
+                Account? account = new Account(_user.PrivateKey, Chain.Kovan);
 
             }
             catch (Exception exception)
