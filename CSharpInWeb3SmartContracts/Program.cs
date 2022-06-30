c57ab951ed9d7182f4b509152666dea4b07325fd
Nikos S <niksal1991@gmail.com>
2022-06-30 20:53:33 +0300

    Implemented ConfigureCORS

--- CSharpInWeb3SmartContracts/Program.cs
+++ CSharpInWeb3SmartContracts/Program.cs
@@ -1,11 +1,28 @@
+using CSharpInWeb3SmartContracts;
 using CSharpInWeb3SmartContracts.Utilities;
 using System.Text.Json.Serialization;
 
+
+
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+//Extension method for configuring CORS
+var lala = builder.Services.ConfigureCORS();
 
 // Add services to the container.
 builder.Services.AddControllers().AddJsonOptions(options =>
             options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+
+//Load Controllers dynamically from DLL
+/*Assembly? assembly = Assembly.LoadFile(@"C:\Users\Nikos\source\repos\LoadDynamicControllers\LoadDynamicControllers\bin\Debug\net6.0\Test.dll");
+if (assembly != null)
+{
+    builder.Services.AddControllers().PartManager.ApplicationParts.Add(new AssemblyPart(assembly));
+    builder.Services.AddMvc().AddApplicationPart(assembly).AddControllersAsServices();
+}*/
+
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,12 +31,13 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.UseInlineDefinitionsForEnums();
     options.SchemaFilter<EnumSchemaFilter>();
-}
-);
+});
+
 
 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ CSharpInWeb3SmartContracts/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpInWeb3SmartContracts
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection ConfigureCORS(this IServiceCollection services)
+            => services.AddCors(options => options.AddPolicy("AllowOrigins", builder => builder
+                .WithOrigins("http://localhost:7093")
+                .AllowAnyMethod()
+                .AllowAnyHeader()));
+    }
+}
