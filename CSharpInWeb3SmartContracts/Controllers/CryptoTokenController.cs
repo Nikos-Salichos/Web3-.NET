b0f0bc958c8b186d771f1195e19e29b79a7351f6
Nikos S <niksal1991@gmail.com>
2022-07-01 07:54:22 +0300

    Implemented GetBalance

--- CSharpInWeb3SmartContracts/Controllers/CryptoTokenController.cs
+++ CSharpInWeb3SmartContracts/Controllers/CryptoTokenController.cs
@@ -70,6 +70,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
                 Account? account = new Account(_user.PrivateKey, chain);
                 Web3? web3 = new Web3(account, EnumHelper.GetStringBasedOnEnum(blockchainNetwork));
 
+                object[]? parameters = new object[1] { address };
             }
             catch (Exception exception)
             {
