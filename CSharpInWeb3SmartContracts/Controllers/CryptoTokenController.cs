fea3dcc0434517f3ae1b6a92dd18cbfd2fc8c2bb
Nikos S <niksal1991@gmail.com>
2022-07-01 07:54:05 +0300

    Implemented GetBalance

--- CSharpInWeb3SmartContracts/Controllers/CryptoTokenController.cs
+++ CSharpInWeb3SmartContracts/Controllers/CryptoTokenController.cs
@@ -67,7 +67,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
         {
             try
             {
-
+                Account? account = new Account(_user.PrivateKey, chain);
             }
             catch (Exception exception)
             {
