aad23821b1333642c4e11fca1fbb0b55401cad97
Nikos S <niksal1991@gmail.com>
2022-07-10 08:39:37 +0300

    Implemented CallContractVariable

--- CSharpInWeb3SmartContracts/Controllers/SmartContractController.cs
+++ CSharpInWeb3SmartContracts/Controllers/SmartContractController.cs
@@ -83,7 +83,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
             try
             {
                 Account? account = new Account(privateKey, chain);
-
+                Web3? web3 = new Web3(account, EnumHelper.GetStringBasedOnEnum(chain));
 
 
             }
