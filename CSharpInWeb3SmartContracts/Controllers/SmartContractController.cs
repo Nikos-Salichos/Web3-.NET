0d03274aead49a2cc43e4c73b234a16d73982b59
Nikos S <niksal1991@gmail.com>
2022-07-12 07:54:44 +0300

    Implemented CallWriteFunction in SmartContractController

--- CSharpInWeb3SmartContracts/Controllers/SmartContractController.cs
+++ CSharpInWeb3SmartContracts/Controllers/SmartContractController.cs
@@ -155,6 +155,13 @@ namespace CSharpInWeb3SmartContracts.Controllers
                         parameters = null;
                     }
                 }
+
+                HexBigInteger? value = null;
+                BigInteger wei = Web3.Convert.ToWei(sendAsEth);
+                if (wei != 0)
+                {
+                    value = new HexBigInteger(wei);
+                }
             }
             catch (Exception exception)
             {
