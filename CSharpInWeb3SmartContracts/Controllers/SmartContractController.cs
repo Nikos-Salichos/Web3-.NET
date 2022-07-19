ff089d51d601a97f80e58d21f5f913a243f777b7
Nikos S <niksal1991@gmail.com>
2022-07-19 07:34:30 +0300

    Implemented TrackCryptoWhalesForAnyToken in SmartContractController

--- CSharpInWeb3SmartContracts/Controllers/SmartContractController.cs
+++ CSharpInWeb3SmartContracts/Controllers/SmartContractController.cs
@@ -124,6 +124,8 @@ namespace CSharpInWeb3SmartContracts.Controllers
                 Account? account = new Account(_user.PrivateKey, chain);
                 Web3? web3 = new Web3(account, EnumHelper.GetStringBasedOnEnum(chain));
                 Contract? smartContract = web3.Eth.GetContract(smartContractModel.Abi.ToString(), smartContractModel.Address);
+
+                Event transferEvent = smartContract.GetEvent("Transfer");
             }
             catch (Exception exception)
             {
