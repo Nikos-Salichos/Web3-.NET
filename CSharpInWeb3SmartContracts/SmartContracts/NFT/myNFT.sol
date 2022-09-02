1df3084937f320eefa7595b2f4de875494486f41
Nikos S <niksal1991@gmail.com>
2022-09-02 07:53:47 +0300

    Added abstract contract Context

---
+++ CSharpInWeb3SmartContracts/SmartContracts/NFT/myNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.16;
+ 
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
