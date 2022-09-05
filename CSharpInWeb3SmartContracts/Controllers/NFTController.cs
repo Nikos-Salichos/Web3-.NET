ccbfeb2e735e1f798f86c6cfeec3d348a8483665
Nikos S <niksal1991@gmail.com>
2022-09-05 05:13:42 +0300

    Implemeneted DeploySmartContract in NFTController

--- CSharpInWeb3SmartContracts/Controllers/NFTController.cs
+++ CSharpInWeb3SmartContracts/Controllers/NFTController.cs
@@ -28,7 +28,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
         {
             try
             {
-
+                Account? account = new Account(_user.PrivateKey, chain);
             }
             catch (Exception exception)
             {
