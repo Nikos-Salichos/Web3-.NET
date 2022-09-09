54500bd73dff59fd94dc591f4a7a5aa0be881949
Nikos S <niksal1991@gmail.com>
2022-09-09 08:06:10 +0300

    Implemeneted Mint in NFTController

--- CSharpInWeb3SmartContracts/Controllers/NFTController.cs
+++ CSharpInWeb3SmartContracts/Controllers/NFTController.cs
@@ -59,7 +59,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
         {
             try
             {
-
+                Account? account = new Account(_user.PrivateKey, chain);
             }
             catch (Exception exception)
             {
