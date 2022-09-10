6e915e8f4f5edafde0f14d71ec21fd715d943f0c
Nikos S <niksal1991@gmail.com>
2022-09-10 08:55:20 +0300

    Implemented GetCoins

--- CSharpInWeb3SmartContracts/Controllers/CoinMarketCapController.cs
+++ CSharpInWeb3SmartContracts/Controllers/CoinMarketCapController.cs
@@ -23,6 +23,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
 
                 var restRequest = new RestRequest();
                 restRequest.Method = Method.Get;
+                restRequest.AddHeader("X-CMC_PRO_API_KEY", _apiKey);
             }
             catch (Exception exception)
             {
