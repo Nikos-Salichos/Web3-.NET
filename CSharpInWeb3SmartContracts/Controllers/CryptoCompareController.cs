88b5e0989561110cf0fe108ee6f142b0c1f43e98
Nikos S <niksal1991@gmail.com>
2022-09-11 08:19:45 +0300

    Added CryptoCompareController

---
+++ CSharpInWeb3SmartContracts/Controllers/CryptoCompareController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpInWeb3SmartContracts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoCompareController : ControllerBase
+    {
+        private string _apiKey { get; }
+
+
+    }
+}
