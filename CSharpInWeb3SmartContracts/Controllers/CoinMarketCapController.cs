a97c59a92a117880b569bc9d052a0996be473173
Nikos S <niksal1991@gmail.com>
2022-09-16 21:56:34 +0300

    Implemented GetCategoriesId in CoinMarketCapController

--- CSharpInWeb3SmartContracts/Controllers/CoinMarketCapController.cs
+++ CSharpInWeb3SmartContracts/Controllers/CoinMarketCapController.cs
@@ -61,6 +61,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
                 RestRequest restRequest = new RestRequest();
                 restRequest.Method = Method.Get;
                 restRequest.AddHeader("X-CMC_PRO_API_KEY", ApiKey);
+                restRequest.AddHeader("Accept", "application/json");
             }
             catch (Exception exception)
             {
