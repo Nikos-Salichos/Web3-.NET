5d0ab58e9a075f97229501a8b85ae71f21110f73
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-09-18 09:16:06 +0300

    Update CryptoToken.sol

--- CSharpInWeb3SmartContracts/SmartContracts/CryptoToken/CryptoToken.sol
+++ CSharpInWeb3SmartContracts/SmartContracts/CryptoToken/CryptoToken.sol
@@ -518,6 +518,7 @@ contract CryptoCoinFactory{
     CryptoCoin[] public CryptoCoins;
     function createCryptoCoin(uint256 initialSupply, string memory tokenName, string memory tokenSymbol, uint256 tokenCap) public {
       CryptoCoin cryptoCoin = new CryptoCoin(initialSupply,tokenName,tokenSymbol,tokenCap);
+      CryptoCoins.push(cryptoCoin);
     }
     
 }
