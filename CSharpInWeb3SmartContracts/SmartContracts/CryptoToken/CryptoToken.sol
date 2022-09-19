ff73d9b9a832e9a763410d69af47bf08b93b2f77
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-09-19 07:54:51 +0300

    Update CryptoToken.sol

--- CSharpInWeb3SmartContracts/SmartContracts/CryptoToken/CryptoToken.sol
+++ CSharpInWeb3SmartContracts/SmartContracts/CryptoToken/CryptoToken.sol
@@ -536,4 +536,5 @@ contract CloneFactory {
   mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
   mstore(add(clone, 0x14), targetBytes)
   mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+  result := create(0, clone, 0x37)
 }
