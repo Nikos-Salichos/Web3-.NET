19d04a337381c2d58503765ee4ad4117a0e3cac2
Nikos S <niksal1991@gmail.com>
2022-12-06 18:28:10 +0200

    Added coin factory

--- CSharpInWeb3SmartContracts/SmartContracts/CryptoToken/CryptoToken.sol
+++ CSharpInWeb3SmartContracts/SmartContracts/CryptoToken/CryptoToken.sol
@@ -512,3 +512,62 @@ contract Staking {
 
       receive() external payable{}
 }
+
+
+contract CryptoCoinFactory{ 
+
+    CryptoCoin[] public CryptoCoins;
+
+    function createCryptoCoin(uint256 initialSupply, string memory tokenName, string memory tokenSymbol, uint256 tokenCap) public {
+        CryptoCoin cryptoCoin = new CryptoCoin(initialSupply,tokenName,tokenSymbol,tokenCap);
+        CryptoCoins.push(cryptoCoin);
+    }
+
+    function getAllCryptoCoins() public pure returns (CryptoCoin[] memory allCryptoCoins)
+    {
+        return allCryptoCoins;
+    }
+}
+
+
+contract CloneFactory {
+
+  function createClone(address target) internal returns (address result) {
+    bytes20 targetBytes = bytes20(target);
+    assembly {
+      let clone := mload(0x40)
+      mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+      mstore(add(clone, 0x14), targetBytes)
+      mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+      result := create(0, clone, 0x37)
+    }
+  }
+
+  function isClone(address target, address query) internal view returns (bool result) {
+    bytes20 targetBytes = bytes20(target);
+    assembly {
+      let clone := mload(0x40)
+      mstore(clone, 0x363d3d373d3d3d363d7300000000000000000000000000000000000000000000)
+      mstore(add(clone, 0xa), targetBytes)
+      mstore(add(clone, 0x1e), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+
+      let other := add(clone, 0x40)
+      extcodecopy(query, other, 0, 0x2d)
+      result := and(
+        eq(mload(clone), mload(other)),
+        eq(mload(add(clone, 0xd)), mload(add(other, 0xd)))
+      )
+    }
+  }
+}
+
+contract CryptoCoinFactoryClone is Ownable, CloneFactory {
+
+  address public libraryAddress;
+
+  event CryptoCoinCreated(address newFoundation);
+
+  function CryptoCoinFactoryWithLibrary(address _libraryAddress) public {
+    libraryAddress = _libraryAddress;
+  }
+}
