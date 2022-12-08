b25f9079deb7122e791036d52757f5f96528b519
Nikos S <niksal1991@gmail.com>
2022-12-08 08:41:26 +0200

    Added logger in LotteryController

--- CSharpInWeb3SmartContracts/Controllers/LotteryController.cs
+++ CSharpInWeb3SmartContracts/Controllers/LotteryController.cs
@@ -178,6 +178,7 @@ namespace CSharpInWeb3SmartContracts.Controllers
             }
             catch (Exception exception)
             {
+                _logger.LogError(MethodBase.GetCurrentMethod()?.Name + ' ' + exception);
                 return BadRequest(exception.Message);
             }
         }
