c9233d1b0f8e74f59ce35691f202df6703cd989e
Nikos S <niksal1991@gmail.com>
2022-12-09 21:53:54 +0200

    Moved User to domain project

--- CSharpInWeb3SmartContracts/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CSharpInWeb3SmartContracts.Models
-{
-    public class User
-    {
-        public string? WalletAddress { get; set; }
-        public string? PrivateKey { get; set; }
-    }
-}
