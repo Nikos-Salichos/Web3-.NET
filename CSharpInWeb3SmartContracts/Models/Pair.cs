1704b7bec06b9c1fa30f453913d0c5338d1f15ee
Nikos S <niksal1991@gmail.com>
2022-12-09 21:54:30 +0200

    Moved Pair and SmartContractModel

--- CSharpInWeb3SmartContracts/Models/Pair.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CSharpInWeb3SmartContracts.Models
-{
-    public class Pair
-    {
-        public string Id { get; set; } = string.Empty;
-        public Token Token0 { get; set; } = new Token();
-        public Token Token1 { get; set; } = new Token();
-    }
-}
