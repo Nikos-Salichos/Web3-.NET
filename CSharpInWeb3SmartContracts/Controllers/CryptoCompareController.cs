e45408070ce0240ab6cdd35265d7abf862497058
Nikos S <niksal1991@gmail.com>
2022-12-11 08:50:53 +0200

    Removed try catch from CryptoCompareController

--- CSharpInWeb3SmartContracts/Controllers/CryptoCompareController.cs
+++ CSharpInWeb3SmartContracts/Controllers/CryptoCompareController.cs
@@ -19,62 +19,55 @@ namespace WebApi.Controllers
         [HttpGet("GetCoins")]
         public async Task<ActionResult> GetCoins()
         {
-            try
+            List<Coin> coins = new();
+
+            for (int i = 0; i < 2; i++)
             {
-                List<Coin> coins = new();
+                RestClient restClient = new($"https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&page={i}&tsym=USD&api_key={ApiKey}");
 
-                for (int i = 0; i < 2; i++)
+                RestRequest restRequest = new();
+                restRequest.Method = Method.Get;
+
+                RestResponse response = await restClient.ExecuteAsync(restRequest, default);
+
+                if (response is null)
                 {
-                    RestClient restClient = new($"https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&page={i}&tsym=USD&api_key={ApiKey}");
-
-                    RestRequest restRequest = new();
-                    restRequest.Method = Method.Get;
-
-                    RestResponse response = await restClient.ExecuteAsync(restRequest, default);
-
-                    if (response is null)
-                    {
-                        return NotFound("Response is null");
-                    }
-
-                    if (string.IsNullOrEmpty(response.Content))
-                    {
-                        return NotFound("No response content found");
-                    }
-
-                    CryptoCompare? cryptoCompare = JsonConvert.DeserializeObject<CryptoCompare>(response.Content);
-
-                    if (cryptoCompare is null)
-                    {
-                        return NotFound("Response is empty");
-                    }
-
-                    if (cryptoCompare.Data is null)
-                    {
-                        return NotFound("Response Data is empty");
-                    }
-
-                    foreach (var cryptoCoin in cryptoCompare.Data)
-                    {
-                        Coin coin = new()
-                        {
-                            Id = cryptoCoin?.CoinInfo?.Id,
-                            Name = cryptoCoin?.CoinInfo?.Name,
-                            FullName = cryptoCoin?.CoinInfo?.FullName,
-                            MarketCap = cryptoCoin?.RAW?.USD?.MKTCAP
-                        };
-                        coins.Add(coin);
-                    }
+                    return NotFound("Response is null");
                 }
 
-                List<Coin> sortedCoins = coins.OrderByDescending(c => c.MarketCap).ToList();
+                if (string.IsNullOrEmpty(response.Content))
+                {
+                    return NotFound("No response content found");
+                }
 
-                return Ok(sortedCoins);
+                CryptoCompare? cryptoCompare = JsonConvert.DeserializeObject<CryptoCompare>(response.Content);
+
+                if (cryptoCompare is null)
+                {
+                    return NotFound("Response is empty");
+                }
+
+                if (cryptoCompare.Data is null)
+                {
+                    return NotFound("Response Data is empty");
+                }
+
+                foreach (var cryptoCoin in cryptoCompare.Data)
+                {
+                    Coin coin = new()
+                    {
+                        Id = cryptoCoin?.CoinInfo?.Id,
+                        Name = cryptoCoin?.CoinInfo?.Name,
+                        FullName = cryptoCoin?.CoinInfo?.FullName,
+                        MarketCap = cryptoCoin?.RAW?.USD?.MKTCAP
+                    };
+                    coins.Add(coin);
+                }
             }
-            catch (Exception exception)
-            {
-                return BadRequest(exception.Message);
-            }
+
+            List<Coin> sortedCoins = coins.OrderByDescending(c => c.MarketCap).ToList();
+
+            return Ok(sortedCoins);
         }
     }
 }
