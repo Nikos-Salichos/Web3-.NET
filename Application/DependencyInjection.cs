2c2d012e1266be44fe5bbc0529d26e906a9cf57d
Nikos S <niksal1991@gmail.com>
2022-12-18 08:56:18 +0200

    Added SmartContractDTO

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationCore(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
---
+++ Domain/DTOs/SmartContractDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.DTOs
+{
+    public class SmartContractDTO
+    {
+        public string? Address { get; set; }
+        public string? Bytecode { get; set; }
+        public object? Abi { get; set; }
+        public object? Parameters { get; set; }
+    }
+}
