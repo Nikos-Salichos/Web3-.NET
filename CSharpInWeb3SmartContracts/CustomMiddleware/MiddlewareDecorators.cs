e6b1f31161f89b2c22b72372c9259334aa6acce6
Nikos S <niksal1991@gmail.com>
2022-12-22 19:22:21 +0200

    Revert "Revert "Added MiddlewareDecorators""
    This reverts commit 50e599c7bc7df382374d06e71ea0d260af92b2ca.

---
+++ CSharpInWeb3SmartContracts/CustomMiddleware/MiddlewareDecorators.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi.CustomMiddleware
+{
+    public class MiddlewareDecorators
+    {
+        [AttributeUsage(AttributeTargets.Method)]
+        public class LimitRequest : Attribute
+        {
+            public int TimeWindow
+            {
+                get;
+                set;
+            }
+            public int MaxRequests
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
