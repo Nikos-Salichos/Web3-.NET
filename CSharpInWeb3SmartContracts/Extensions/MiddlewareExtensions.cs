a1053dc0f31a020801817b02d35c866661632604
Nikos S <niksal1991@gmail.com>
2022-12-22 19:22:48 +0200

    Removed Middleware

---
+++ CSharpInWeb3SmartContracts/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using WebApi.CustomMiddleware;
+
+namespace WebApi.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static void UseGlobalExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware(typeof(ExceptionHandlingMiddleware));
+        }
+    }
+}
