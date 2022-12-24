6fd23fa1c1d49ea4032724fb3a5e264cd96d657b
Nikos S <niksal1991@gmail.com>
2022-12-24 09:32:03 +0200

    Added _httpClient

--- CSharpInWeb3SmartContracts/Controllers/ProxyController.cs
+++ CSharpInWeb3SmartContracts/Controllers/ProxyController.cs
@@ -8,21 +8,5 @@ namespace WebApi.Controllers
     public class ProxyController : ControllerBase
     {
         private readonly HttpClient _httpClient;
-
-        public ProxyController(IHttpClientFactory httpclient)
-        {
-            _httpClient = httpclient.CreateClient();
-        }
-
-        private async Task<ContentResult> ProxyTo(string url)
-        {
-            return Content(await _httpClient.GetStringAsync(url));
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> SmartContracts()
-        {
-            return await ProxyTo("https://localhost:7093/GetAllSmartContracts");
-        }
     }
 }
