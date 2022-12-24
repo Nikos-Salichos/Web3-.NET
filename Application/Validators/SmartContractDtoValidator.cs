362978ff263424ec308e281d83512b981302ebb4
Nikos S <niksal1991@gmail.com>
2022-12-24 11:44:47 +0200

    Added validation for smartContractDto

--- Application/Validators/SmartContractDtoValidator.cs
+++ Application/Validators/SmartContractDtoValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using Domain.DTOs;
+using FluentValidation;
 
 namespace Application.Validators
 {
--- Domain/DTOs/SmartContractDTO.cs
+++ Domain/DTOs/SmartContractDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace Domain.DTOs
+﻿using Nethereum.Signer;
+
+namespace Domain.DTOs
 {
     public class SmartContractDTO
     {
         public string? Address { get; set; }
         public string? Bytecode { get; set; }
+        public Chain Chain { get; set; } = new Chain();
         public object? Abi { get; set; }
         public List<object>? Parameters { get; set; }
     }
