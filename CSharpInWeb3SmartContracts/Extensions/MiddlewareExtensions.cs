82e4eead1376af7d642eec7232ba2bff4197051d
Nikos S <niksal1991@gmail.com>
2022-12-24 10:33:45 +0200

    Registered AddRateLimiting

--- CSharpInWeb3SmartContracts/Extensions/MiddlewareExtensions.cs
+++ CSharpInWeb3SmartContracts/Extensions/MiddlewareExtensions.cs
@@ -17,7 +17,29 @@ namespace WebApi.Extensions
 
         public static void AddRateLimiting(this IServiceCollection services)
         {
-
+            services.AddMemoryCache();
+            services.Configure<IpRateLimitOptions>(options =>
+            {
+                options.EnableEndpointRateLimiting = true;
+                options.StackBlockedRequests = false;
+                options.HttpStatusCode = 429;
+                options.RealIpHeader = "X-Real-IP";
+                options.ClientIdHeader = "X-ClientId";
+                options.GeneralRules = new List<RateLimitRule>
+                {
+                    new RateLimitRule
+                    {
+                        Endpoint = "GET:/GetAllSmartContracts",
+                        Period = "2s",
+                        Limit = 2,
+                    }
+                };
+            });
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+            services.AddInMemoryRateLimiting();
         }
     }
 }
