f0455d0cfd8164d530d19fb7f641d80fd4d464f1
Nikos S <niksal1991@gmail.com>
2022-12-25 09:32:50 +0200

    Added HasColumnOrder on property Id

---
+++ Infrastructure/Migrations/20221225073122_DB Initialize.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DBInitialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SmartContract",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Bytecode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Chain = table.Column<int>(type: "int", nullable: false),
+                    Abi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Parameters = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SmartContract", x => x.Address);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SmartContract");
+        }
+    }
+}
