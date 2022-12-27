71d426b7e2cdb542ab8aa427cd18d811fed50b08
Nikos S <niksal1991@gmail.com>
2022-12-27 08:19:44 +0200

    Added _fallbackPolicy

--- CSharpInWeb3SmartContracts/Controllers/ProxyController.cs
+++ CSharpInWeb3SmartContracts/Controllers/ProxyController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Polly;
+using Polly.CircuitBreaker;
 using Polly.Fallback;
+using Polly.Retry;
+using Polly.Wrap;
 
 namespace WebApi.Controllers
 {
@@ -10,14 +14,31 @@ namespace WebApi.Controllers
     {
         private readonly HttpClient _httpClient;
         private readonly AsyncFallbackPolicy<IActionResult> _fallbackPolicy;
+        private readonly AsyncRetryPolicy<IActionResult> _retryPolicy;
+        private readonly AsyncCircuitBreakerPolicy _circuitBreakerPolicy;
+        private readonly AsyncPolicyWrap<IActionResult> _policy;
+
         public ProxyController(IHttpClientFactory httpclient)
         {
             _httpClient = httpclient.CreateClient();
+
+            _fallbackPolicy = Policy<IActionResult>
+                .Handle<Exception>()
+                .FallbackAsync(Content("Sorry, we are currently experiencing issues. Please try again later"));
+
+            _retryPolicy = Policy<IActionResult>.Handle<Exception>().RetryAsync();
+
+            _circuitBreakerPolicy ??= Policy.Handle<Exception>().CircuitBreakerAsync(2, TimeSpan.FromMinutes(1));
+
+            _policy = Policy<IActionResult>.Handle<Exception>()
+                                           .FallbackAsync(Content("Sorry, we are currently experiencing issues. Please try again later"))
+                                           .WrapAsync(_retryPolicy)
+                                           .WrapAsync(_circuitBreakerPolicy);
         }
 
-        private async Task<ContentResult> ProxyTo(string url)
+        private async Task<IActionResult> ProxyTo(string url)
         {
-            return Content(await _httpClient.GetStringAsync(url));
+            return await _fallbackPolicy.ExecuteAsync(async () => Content(await _httpClient.GetStringAsync(url)));
         }
 
         [HttpGet]
@@ -25,6 +46,7 @@ namespace WebApi.Controllers
         {
             return await ProxyTo("https://localhost:7093/GetAllSmartContracts");
         }
+
 
     }
 }
