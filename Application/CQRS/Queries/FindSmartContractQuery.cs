d6b63c0f70ca8241662ae03d4f3a95b8c7d1cfc7
Nikos S <niksal1991@gmail.com>
2022-12-29 21:33:58 +0200

    Added FindSmartContractQuery

---
+++ Application/CQRS/Queries/FindSmartContractQuery.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using MediatR;
+using System.Linq.Expressions;
+
+namespace Application.CQRS.Queries
+{
+    public class FindSmartContractQuery : IRequest<SmartContract>
+    {
+        public Expression<Func<SmartContract, bool>> Predicate { get; set; }
+
+        public FindSmartContractQuery(Expression<Func<SmartContract, bool>> predicate)
+        {
+            Predicate = predicate;
+        }
+    }
+}
+
+
