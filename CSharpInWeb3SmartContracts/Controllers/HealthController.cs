1cc921c8a9439723d6d7adfd006759cf1e6f8743
Nikos S <niksal1991@gmail.com>
2022-12-30 19:52:02 +0200

    Added _healthCheckService

--- CSharpInWeb3SmartContracts/Controllers/HealthController.cs
+++ CSharpInWeb3SmartContracts/Controllers/HealthController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace WebApi.Controllers
 {
     public class HealthController : ControllerBase
     {
         private readonly ILogger<HealthController> _logger;
+        private readonly HealthCheckService _healthCheckService;
     }
 }
