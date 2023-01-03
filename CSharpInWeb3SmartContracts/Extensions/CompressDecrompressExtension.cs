9e1961f6aec95636f087c2aff6bbea0dbd1a92d3
Nikos S <niksal1991@gmail.com>
2023-01-03 18:56:28 +0200

    using Memory Stream

--- CSharpInWeb3SmartContracts/Extensions/CompressDecrompressExtension.cs
+++ CSharpInWeb3SmartContracts/Extensions/CompressDecrompressExtension.cs
@@ -1,10 +1,17 @@
-﻿namespace WebApi.Extensions
+﻿using System.IO.Compression;
+
+namespace WebApi.Extensions
 {
     public static class CompressDecrompressExtension
     {
         public static byte[] Compress(byte[] bytes)
         {
-
+            using var memoryStream = new MemoryStream();
+            using (var brotliStream = new BrotliStream(memoryStream, CompressionLevel.Optimal))
+            {
+                brotliStream.Write(bytes, 0, bytes.Length);
+            }
+            return memoryStream.ToArray();
         }
     }
 }
