47a90d3777cf6b5f3e22c58f2287d07f4e129c36
Nikos S <niksal1991@gmail.com>
2023-01-07 14:28:11 +0200

    Added Property Attributes

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -1,8 +1,13 @@
-﻿namespace Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
 {
     public class User
     {
+        [Required]
         public string? WalletAddress { get; set; }
+
+        [Required]
         public string? PrivateKey { get; set; }
     }
 }
