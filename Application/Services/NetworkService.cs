df09071b0281c2886470b5e62eeac188cb7d496b
Nikos S <niksal1991@gmail.com>
2023-01-08 18:50:32 +0200

    Implemented INetworkService methods

--- Application/Services/NetworkService.cs
+++ Application/Services/NetworkService.cs
@@ -1,8 +1,23 @@
 ﻿using Application.Interfaces;
+using Nethereum.RPC.Eth.DTOs;
 
 namespace Application.Services
 {
     public class NetworkService : INetworkService
     {
+        public Task<IEnumerable<Transaction>> GetAllContractCreationTransactionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlockWithTransactionHashes> GetLatestBlockAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Transaction[]> GetTransactionsOfABlock()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
