81c068c45b33f9cc8a10382b531cbfe5a6c5030e
Nikos S <niksal1991@gmail.com>
2023-01-09 21:48:05 +0200

    Added networkConfig ctor

--- Application/Services/SingletonOptionsService.cs
+++ Application/Services/SingletonOptionsService.cs
@@ -9,9 +9,10 @@ namespace Application.Services
         private readonly IOptionsMonitor<User> _userOptions;
         private readonly IOptionsMonitor<NetworkConfig> _networkConfig;
 
-        public SingletonOptionsService(IOptionsMonitor<User> userOptions)
+        public SingletonOptionsService(IOptionsMonitor<User> userOptions, IOptionsMonitor<NetworkConfig> networkConfig)
         {
             _userOptions = userOptions;
+            _networkConfig = networkConfig;
         }
 
         public NetworkConfig GetNetworkConfig()
