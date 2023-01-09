85cb8814b76d4c0026ceb306ea98f8e510acb5fd
Nikos S <niksal1991@gmail.com>
2023-01-09 21:47:39 +0200

    Added _networkConfig

--- Application/Services/SingletonOptionsService.cs
+++ Application/Services/SingletonOptionsService.cs
@@ -7,10 +7,16 @@ namespace Application.Services
     public class SingletonOptionsService : ISingletonOptionsService
     {
         private readonly IOptionsMonitor<User> _userOptions;
+        private readonly IOptionsMonitor<NetworkConfig> _networkConfig;
 
         public SingletonOptionsService(IOptionsMonitor<User> userOptions)
         {
             _userOptions = userOptions;
+        }
+
+        public NetworkConfig GetNetworkConfig()
+        {
+            throw new NotImplementedException();
         }
 
         public User GetUserSettings()
