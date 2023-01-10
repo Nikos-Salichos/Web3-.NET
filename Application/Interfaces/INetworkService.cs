3b6f8f8e10b60647b56e898f0d3163ba5d289cb2
Nikos S <niksal1991@gmail.com>
2023-01-10 18:55:59 +0200

    Changed method parameter

--- Application/Interfaces/INetworkService.cs
+++ Application/Interfaces/INetworkService.cs
@@ -7,7 +7,7 @@ namespace Application.Interfaces
     public interface INetworkService
     {
         Task<BlockWithTransactionHashes> GetBlockAsync(BigInteger blockNumber, Chain chain);
-        Task<Transaction[]> GetTransactionsOfABlock();
+        Task<Transaction[]> GetTransactionsOfABlock(long blockNumber, Chain chain);
         Task<IEnumerable<Transaction>> GetAllContractCreationTransactionsAsync();
     }
 }
