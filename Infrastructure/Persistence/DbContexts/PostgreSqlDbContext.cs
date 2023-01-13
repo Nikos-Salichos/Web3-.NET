1d86574af4eedb6cb29c5a6a535d0b0af0a78675
Nikos S <niksal1991@gmail.com>
2023-01-13 23:12:56 +0200

    Added constructor with base

--- Infrastructure/Persistence/DbContexts/PostgreSqlDbContext.cs
+++ Infrastructure/Persistence/DbContexts/PostgreSqlDbContext.cs
@@ -5,6 +5,7 @@ namespace Infrastructure.Persistence.DbContexts
 {
     public class PostgreSqlDbContext : DbContext, IPostgreSqlDbContext
     {
+        public PostgreSqlDbContext(DbContextOptions<PostgreSqlDbContext> options) : base(options) { }
 
     }
 }
