7630831edf740e5a420ac2c2b4425ec8d0cc8628
Nikos S <niksal1991@gmail.com>
2023-01-14 18:12:06 +0200

    Added RateLimitOptions model

---
+++ Domain/Models/RateLimitOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models
+{
+    public class RateLimitOptions
+    {
+        public bool AutoReplenishment { get; set; }
+        public int PermitLimit { get; set; }
+        public int Window { get; set; }
+        public int QueueLimit { get; set; }
+    }
+}
