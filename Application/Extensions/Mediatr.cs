6ade5a47e9c6c32fa2b152d80ae610f03d45a7a8
Nikos S <niksal1991@gmail.com>
2023-01-15 17:59:09 +0200

    Change namespace

--- Application/Extensions/Mediatr.cs
+++ Application/Extensions/Mediatr.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
-namespace Application.RegisterServices
+namespace Application.Extensions
 {
     public static class Mediatr
     {
