32c062092fc905878039c2c0e05b74a569f4e9dc
Nikos S <niksal1991@gmail.com>
2023-01-18 08:36:04 +0200

    Mock Ilogger

--- Tests/UnitTests/SmartContractControllerUnitTests.cs
+++ Tests/UnitTests/SmartContractControllerUnitTests.cs
@@ -102,6 +102,9 @@ namespace Tests.UnitTests
 
             Mock<IConfiguration> mockConfig = new Mock<IConfiguration>();
             mockConfig.Setup(x => x.GetSection(It.Is<string>(k => k == "User"))).Returns(mockSection.Object);
+
+            var mockLogger = new Mock<ILogger<SmartContractController>>();
+
         }
 
     }
