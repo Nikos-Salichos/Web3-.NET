fcbc61a9ec2c642ff0b66ccb1327c2a99cb201ec
Nikos S <niksal1991@gmail.com>
2023-01-22 06:03:22 +0200

    Changed WalletOwner

--- Domain/Models/WalletOwner.cs
+++ Domain/Models/WalletOwner.cs
@@ -5,9 +5,15 @@ namespace Domain.Models
     public class WalletOwner
     {
         [Required]
-        public string? WalletAddress { get; set; }
+        public string WalletAddress { get; set; } = string.Empty;
 
         [Required]
-        public string? PrivateKey { get; set; }
+        public string PrivateKey { get; set; } = string.Empty;
+
+        public IEnumerable<object> GetEqualityComponents()
+        {
+            yield return WalletAddress;
+            yield return PrivateKey;
+        }
     }
 }
