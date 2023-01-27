7439e4714a8f276aa836ce99026db5f717617606
Nikos S <niksal1991@gmail.com>
2023-01-27 09:36:09 +0200

    Added IDistirbutedCache to SmartContractRepository

--- Infrastructure/Persistence/Repositories/SmartContractRepository.cs
+++ Infrastructure/Persistence/Repositories/SmartContractRepository.cs
@@ -1,13 +1,21 @@
 ﻿using Domain.Models;
+using Infrastructure.Persistence.Cache;
 using Infrastructure.Persistence.DbContexts;
 using Infrastructure.Persistence.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
 using System.Linq.Expressions;
 
 namespace Infrastructure.Persistence.Repositories
 {
     public class SmartContractRepository : GenericRepository<SmartContract>, ISmartContractRepository
     {
-        public SmartContractRepository(MsqlDbContext smartContractDbContext) : base(smartContractDbContext) { }
+        private readonly IDistributedCache _distributedCache;
+
+        public SmartContractRepository(MsqlDbContext smartContractDbContext, IDistributedCache distributedCache)
+            : base(smartContractDbContext, distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
 
         public async Task<IEnumerable<SmartContract>> GetSmartContracts()
         {
@@ -17,7 +25,13 @@ namespace Infrastructure.Persistence.Repositories
 
         public async Task<SmartContract> GetSmartContractAsync(long id)
         {
+            var cacheSmartContract = await _distributedCache.GetRecordAsync<SmartContract>(id.ToString());
+            if (cacheSmartContract != null)
+            {
+                return cacheSmartContract;
+            }
             var smartContract = await GetById(id);
+            await _distributedCache.SetRecordAsync(id.ToString(), smartContract);
             return smartContract;
         }
 
