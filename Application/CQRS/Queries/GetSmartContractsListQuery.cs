a2f64ef53cd621c6faf4f3ffdbe3199b879e2e1f
Nikos S <niksal1991@gmail.com>
2023-02-08 08:49:39 +0200

    Implemented PageSize and PageNumber GetSmartContractsListQuery

--- Application/CQRS/Queries/GetSmartContractsListQuery.cs
+++ Application/CQRS/Queries/GetSmartContractsListQuery.cs
@@ -5,5 +5,13 @@ namespace Application.CQRS.Queries
 {
     public class GetSmartContractsListQuery : IRequest<IEnumerable<SmartContract>>
     {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+
+        public GetSmartContractsListQuery(int pageSize, int pageNumber)
+        {
+            PageSize = pageSize;
+            PageNumber = pageNumber;
+        }
     }
 }
