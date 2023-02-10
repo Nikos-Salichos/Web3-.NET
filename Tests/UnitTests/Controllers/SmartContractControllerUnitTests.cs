6e84ae8e1f78bbe951adab8606aca56e60979099
Nikos S <niksal1991@gmail.com>
2023-02-10 08:47:07 +0200

    Refactored FindSmartContractByAddressAsync_ReturnsEmptyResult

--- Tests/UnitTests/Controllers/SmartContractControllerUnitTests.cs
+++ Tests/UnitTests/Controllers/SmartContractControllerUnitTests.cs
@@ -196,11 +196,10 @@ namespace Tests.UnitTests.Controllers
             _mockSmartContractService.Setup(x => x.FindSmartContractsByAddressAsync(IsAny<Expression<Func<SmartContract, bool>>>()))
                .ReturnsAsync(smartContracts);
 
-            Mock<IConfigurationSection> mockSection = new Mock<IConfigurationSection>();
-            mockSection.Setup(x => x.Value).Returns("User");
+            _mockConfigurationSection.Setup(x => x.Value).Returns("User");
 
             Mock<IConfiguration> mockConfig = new Mock<IConfiguration>();
-            mockConfig.Setup(x => x.GetSection(Is<string>(k => k == "User"))).Returns(mockSection.Object);
+            mockConfig.Setup(x => x.GetSection(Is<string>(k => k == "User"))).Returns(_mockConfigurationSection.Object);
 
             var mockLogger = new Mock<ILogger<SmartContractController>>();
 
