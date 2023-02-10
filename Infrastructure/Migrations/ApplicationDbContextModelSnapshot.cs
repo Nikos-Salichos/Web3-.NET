0bfbe18a8bef2cbb86a75aa2dd0ed9b7c4a722cf
Nikos S <niksal1991@gmail.com>
2023-02-11 00:42:01 +0200

    Refactored sql

--- Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Migrations
 {
-    [DbContext(typeof(MsqlDbContext))]
+    [DbContext(typeof(MsSqlDbContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
