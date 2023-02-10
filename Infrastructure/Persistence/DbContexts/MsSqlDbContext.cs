2c9c93d0b2df73a19343cef57c4265ba5aecf201
Nikos S <niksal1991@gmail.com>
2023-02-11 00:42:26 +0200

    Changed MsSqlDbContext

---
+++ Infrastructure/Persistence/DbContexts/MsSqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using Infrastructure.Persistence.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.DbContexts
+{
+    public class MsSqlDbContext : DbContext, IMsSqlDbContext
+    {
+        public MsSqlDbContext(DbContextOptions<MsSqlDbContext> options) : base(options) { }
+
+        public DbSet<SmartContract> SmartContract { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MsSqlDbContext).Assembly);
+            modelBuilder.ApplyConfiguration(new SmartContractConfiguration());
+        }
+    }
+}
