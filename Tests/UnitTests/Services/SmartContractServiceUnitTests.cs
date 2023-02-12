364461c771a3bb75f4ba52ded893137e18c52b61
Nikos S <niksal1991@gmail.com>
2023-02-12 11:01:10 +0200

    Added _mockMediator

--- Tests/UnitTests/Services/SmartContractServiceUnitTests.cs
+++ Tests/UnitTests/Services/SmartContractServiceUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Interfaces;
+using MediatR;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Moq;
 using Xunit;
@@ -10,5 +11,8 @@ namespace Tests.UnitTests.Repositories
     {
         private static readonly Mock<ISmartContractService> _mockSmartContractService = new Mock<ISmartContractService>();
         private static readonly Mock<ISingletonOptionsService> _mockOptionsService = new Mock<ISingletonOptionsService>();
+        private static readonly Mock<IMediator> _mockMediator = new Mock<IMediator>();
+
+
     }
 }
