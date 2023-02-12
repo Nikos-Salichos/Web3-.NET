d1615e9b8cfb659ed34f356601f152595776b8a6
Nikos S <niksal1991@gmail.com>
2023-02-12 11:00:43 +0200

    Added SmartContractServiceUnitTests

---
+++ Tests/UnitTests/Services/SmartContractServiceUnitTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Xunit;
+
+namespace Tests.UnitTests.Repositories
+{
+    [Trait("SmartContractsService", "UnitTests")]
+    public class SmartContractServiceUnitTests
+    {
+
+
+    }
+}
