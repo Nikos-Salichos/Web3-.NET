6706641f93b16437ff4b806c4b7cccde2a900ecd
Nikos S <niksal1991@gmail.com>
2023-02-13 08:19:35 +0200

    Changed interface methods

--- Application/Interfaces/ISmartContractService.cs
+++ Application/Interfaces/ISmartContractService.cs
@@ -8,8 +8,8 @@ namespace Application.Interfaces
     public interface ISmartContractService
     {
         Task<IEnumerable<SmartContractDTO>> GetSmartContractsAsync(int pageSize, int pageNumber);
-        Task<SmartContractDTO?> GetSmartContractByIdAsync(long id);
-        Task<IEnumerable<SmartContractDTO>> FindSmartContractsByAddressAsync(Expression<Func<SmartContract, bool>> predicate);
+        Task<SmartContractDTO> GetSmartContractAsync(long id);
+        Task<IEnumerable<SmartContractDTO>> GetSmartContractsAsync(Expression<Func<SmartContract, bool>> predicate);
         Task<TransactionReceipt> DeploySmartContractAsync(SmartContractDTO smartContract);
         Task<dynamic> ReadContractFunctionVariableAsync(string variableName, SmartContract smartContractJson);
         Task<dynamic> WriteContractFunctionVariableAsync(string variableName, long sendAsEth, SmartContract smartContractJson);
