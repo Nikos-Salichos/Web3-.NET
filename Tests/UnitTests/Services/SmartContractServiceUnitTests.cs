db8aaf9372dbafb35c1424ae9f1300d1e9d25b24
Nikos S <niksal1991@gmail.com>
2023-02-13 08:20:42 +0200

    Added GetSmartContractsAsync_ShouldReturnEmptyList

--- Tests/UnitTests/Services/SmartContractServiceUnitTests.cs
+++ Tests/UnitTests/Services/SmartContractServiceUnitTests.cs
@@ -21,6 +21,13 @@ namespace Tests.UnitTests.Repositories
         private static readonly Mock<IMapper> _mockMapper = new Mock<IMapper>();
 
         [Fact]
+        public async Task GetSmartContractsAsync_ShouldReturnEmptyList()
+        {
+            // Arrange
+            var smartContracts = new List<SmartContract>();
+        }
+
+        [Fact]
         public async Task GetSmartContractsAsync_ShouldReturnSmartContracts()
         {
             // Arrange
@@ -37,15 +44,65 @@ namespace Tests.UnitTests.Repositories
             _mockMapper.Setup(m => m.Map<List<SmartContract>, List<SmartContractDTO>>(IsAny<List<SmartContract>>())).Returns(smartContractsDtos);
             _mockSmartContractService.Setup(m => m.GetSmartContractsAsync(IsAny<int>(), IsAny<int>())).ReturnsAsync(smartContractsDtos);
 
-            var service = new SmartContractService(_mockMapper.Object, _mockMediator.Object, _mockOptionsService.Object);
+            var smartContractService = new SmartContractService(_mockMapper.Object, _mockMediator.Object, _mockOptionsService.Object);
 
             // Act
-            var result = await service.FindSmartContractsByAddressAsync(sc => sc.Address == "SmartContract1");
+            var result = await smartContractService.GetSmartContractsAsync(IsAny<int>(), IsAny<int>());
 
             // Assert
             Assert.NotNull(result);
             Assert.True(smartContracts.Select(x => new { x.Id, x.Address }).SequenceEqual(result.Select(x => new { x.Id, x.Address })));
         }
 
+        [Fact]
+        public async Task GetSmartContractByIdAsync_ShouldReturnSmartContracts()
+        {
+            // Arrange
+            var smartContracts = new List<SmartContract> { new SmartContract { Id = 1, Address = "SmartContract1" } };
+
+            var smartContractsDtos = new List<SmartContractDTO> {
+                new SmartContractDTO { Id = smartContracts.FirstOrDefault().Id, Address = smartContracts.First().Address }
+            };
+
+            var query = new GetSmartContractQuery(IsAny<int>());
+            _mockMediator.Setup(m => m.Send(query, default)).ReturnsAsync(smartContracts.FirstOrDefault());
+            _mockMapper.Setup(m => m.Map<SmartContract, SmartContractDTO>(IsAny<SmartContract>())).Returns(smartContractsDtos.FirstOrDefault());
+            _mockSmartContractService.Setup(m => m.GetSmartContractAsync(1)).ReturnsAsync(smartContractsDtos.FirstOrDefault(x => x.Id == 1));
+
+            var smartContractService = new SmartContractService(_mockMapper.Object, _mockMediator.Object, _mockOptionsService.Object);
+
+            // Act
+            var actualSmartContractDTO = await smartContractService.GetSmartContractAsync(1);
+
+            // Assert
+            Assert.NotNull(actualSmartContractDTO);
+            Assert.IsType<SmartContractDTO>(actualSmartContractDTO);
+            Assert.Equal(actualSmartContractDTO.Id, smartContractsDtos.FirstOrDefault().Id);
+            Assert.Equal(actualSmartContractDTO.Address, smartContractsDtos.FirstOrDefault().Address);
+        }
+
+        [Fact]
+        public async Task GetSmartContractByIdAsync_ShouldReturnEmptySmartContract()
+        {
+            // Arrange
+            var smartContracts = new List<SmartContract>();
+
+            var smartContractsDtos = new List<SmartContractDTO>();
+
+            var query = new GetSmartContractQuery(IsAny<int>());
+            _mockMediator.Setup(m => m.Send(query, default)).ReturnsAsync(new SmartContract());
+            _mockMapper.Setup(m => m.Map<SmartContract, SmartContractDTO>(IsAny<SmartContract>())).Returns(new SmartContractDTO());
+            _mockSmartContractService.Setup(m => m.GetSmartContractAsync(100)).ReturnsAsync(smartContractsDtos.FirstOrDefault(x => x.Id == 100));
+
+            var smartContractService = new SmartContractService(_mockMapper.Object, _mockMediator.Object, _mockOptionsService.Object);
+
+            // Act
+            var actualSmartContractDTO = await smartContractService.GetSmartContractAsync(1);
+
+            // Assert
+            Assert.NotNull(actualSmartContractDTO);
+            Assert.IsType<SmartContractDTO>(actualSmartContractDTO);
+            Assert.Null(actualSmartContractDTO.Address);
+        }
     }
 }
